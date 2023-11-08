ae397de46ae83298696dd453a6b92f6a0caeaf68
yu7400ki <yu7400ki@gmail.com>
2023-11-08 19:01:11 +0900

    biome

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import viteLogo from "/vite.svg";
+import { setupCounter } from "./counter.ts";
+import "./style.css";
+import typescriptLogo from "./typescript.svg";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+// biome-ignore lint/style/noNonNullAssertion:
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="${viteLogo}" class="logo" alt="Vite logo" />
@@ -19,6 +20,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
       Click on the Vite and TypeScript logos to learn more
     </p>
   </div>
-`
+`;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+// biome-ignore lint/style/noNonNullAssertion:
+setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
